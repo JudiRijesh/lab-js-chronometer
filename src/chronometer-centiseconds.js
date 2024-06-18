c9924c59043bfce23ee72ed8f8b03f05a46c2a82
Mary Judi Kavitha Karunakaran <kavitha.rijesh@gmail.com>
2024-06-18 09:45:57 +0200

    Chronometer Exercise and Bonus completed

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,66 @@
-class Chronometer {
+class Chronometer {  
   constructor() {
-    // ... your code goes here
-  }
+  //should initialize two properties for each new chronometer object
+    this.currentTime = 0
+    this.intervalId = null
+}
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    // Set the interval to increment currentTime and invoke printTimeCallback
+   this.intervalId = setInterval(()=>{
+    this.currentTime++ // Incrementing currentTime by 1
+
+// Invoke the callback function
+    if(printTimeCallback){
+      printTimeCallback()
+    }
+// Stop the interval after 200 increments (which should be after 2 seconds)
+   if(this.currentTime>=200){
+    clearInterval(this.intervalId)
+   }
+   // Set the interval to 10 milliseconds (0.01 seconds)
+  },10)
+}
 
   getMinutes() {
-    // ... your code goes here
+// To calculate the minutes, divide the current time by 6000 and use the Math.floor() method to get a rounded number.
+    return Math.floor(this.currentTime/6000) 
   }
 
   getSeconds() {
-    // ... your code goes here
-  }
+
+//return the number of seconds that have passed after the start of the current minute - using the modulo operator (current time % 60) to get the number of remaining seconds
+return Math.floor((this.currentTime%6000)/100) 
+}
 
   getCentiseconds() {
-    // ... your code goes here
+   // Calculate the number of centiseconds within the current second
+   return this.currentTime % 100; 
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+ //taking value as argument above we are returning a string with two digits (by string concatenation step)
+ return ("0" + value).slice(-2)
   }
 
   stop() {
-    // ... your code goes here
+
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+  //reset the current time property to 0
+    this.currentTime = 0
+  }
+  split() {
+    
+let minutes = this.computeTwoDigitNumber(this.getMinutes())
+let seconds = this.computeTwoDigitNumber(this.getSeconds())
+let centiseconds = this.computeTwoDigitNumber(this.getCentiseconds())
+
+//Return value formatted as "mm:ss.cs"
+return `${minutes}:${seconds}.${centiseconds}`
+
+  }
   }
 
-  split() {
-    // ... your code goes here
-  }
-}
--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+//should initialize two properties for each new chronometer object
+  this.currentTime = 0
+  this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+//should increment by 1 the currentTime property on every 1 second interval
+    this.intervalId = setInterval(()=>{
+      this.currentTime++
+//should invoke the passed argument (printTimeCallback) every 1 second
+      if(printTimeCallback){
+        printTimeCallback()}
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+
+// To calculate the minutes, divide the current time by 60 and use the Math.floor() method to get a rounded number.
+    return Math.floor(this.currentTime/60) 
+
   }
 
   getSeconds() {
-    // ... your code goes here
+//return the number of seconds that have passed after the start of the current minute - using the modulo operator (current time % 60) to get the number of remaining seconds
+    return Math.floor(this.currentTime%60) 
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+//taking value as argument above we are returning a string with two digits (by string concatenation step)
+    return ("0" + value).slice(-2)
   }
 
   stop() {
-    // ... your code goes here
+
+// stops the interval
+    clearInterval(this.intervalId)
+    
   }
 
   reset() {
-    // ... your code goes here
+
+    //reset the current time property to 0
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+ 
+//Declare and initialize the minutes and seconds
+
+let minutes = this.computeTwoDigitNumber(this.getMinutes())
+let seconds = this.computeTwoDigitNumber(this.getSeconds())
+
+//Return value formatted as "mm:ss"
+return `${minutes}:${seconds}`
+
   }
 }
